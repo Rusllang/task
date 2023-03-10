a0c157a580616816b17f61e36fa4277750e85d8e
8200830 <gabeevdragon@mail.ru>
2023-03-10 13:29:29 +0300

    4

--- task.py
+++ task.py
@@ -1,19 +1,24 @@
 import json
 
-#1. Из массива удаляются все элементы с четным индексом.
-#2. Все ключи словаря переводятся в верхний регистр.
+
 #3. Из каждой строки удаляется вторая половина символов.
 #4. Остальные элементы остаются без изменений.
 
 def beb(d):
     for j in range(len(d)):
-        if isinstance(d[j], list):
+        if isinstance(d[j], list):#1. Из массива удаляются все элементы с четным индексом.
         
             s = ' '.join([str(d[j][i]) for i in range(len(d[0])) if i % 2 ==1])    
             print(s)
             d[j]=s
-        elif isinstance(d[j], dict):
+        elif isinstance(d[j], dict):#2. Все ключи словаря переводятся в верхний регистр.
+            print("bebra")
+            print(d[j].keys())
+            for i in d[j]:
+                d[j][i].upper()
             print(d[j])
+#        elif isinstance(d[j], str):
+
 
 
 with open("test.json", "r+") as f:
