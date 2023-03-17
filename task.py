79d3d370fb7709c08c7b017584b4b0ea8c836791
8200830 <gabeevdragon@mail.ru>
2023-03-17 05:30:39 +0300

    5

--- task.py
+++ task.py
@@ -1,34 +1,32 @@
 import json
+# -*- coding: utf-8 -*-
 
-
-#3. Из каждой строки удаляется вторая половина символов.
-#4. Остальные элементы остаются без изменений.
-
-def beb(d):
-    for j in range(len(d)):
-        if isinstance(d[j], list):#1. Из массива удаляются все элементы с четным индексом.
+#def beb(d):
+#    for j in range(len(d)):
+#        if isinstance(d[j], list):#1. Из массива удаляются все элементы с четным индексом.
         
-            s = ' '.join([str(d[j][i]) for i in range(len(d[0])) if i % 2 ==1])    
-            print(s)
-            d[j]=s
-        elif isinstance(d[j], dict):#2. Все ключи словаря переводятся в верхний регистр.
-            print("bebra")
-
-            print(d[j].keys())
-            for i in d[j]:
-                print(d[j])
-                i.upper()
-                #i = q
-                #print(q)
-            print(d[j].fromkeys(0))
+#            s = ' '.join([str(d[j][i]) for i in range(len(d[0])) if i % 2 ==1])    
+            
+#            d[j]=s 
+#        elif isinstance(d[j], dict):#2. Все ключи словаря переводятся в верхний регистр.
+#            q = dict((k.upper(), v) for k, v in d[j].items()) 
+#            d[j] = q
 #        elif isinstance(d[j], str):
-    print(d)
+#            p = str(d[j])
+#            l = int(len(p)/2)
+#            #s = p[0:l]
+#            d[j] = s
+#    print(d)
+#    return d
 
 
 with open("test.json", "r+") as f:
     t = json.load(f)
     print(len(t[0]))
     #list s = []
-    d = t
-    beb(d)
+    #d = t
+    #beb(d)
+    #d = t
+    #f.close()
+    #f.write(json.dumps(t))
     
